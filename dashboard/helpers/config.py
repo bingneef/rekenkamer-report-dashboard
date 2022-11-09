96ba7bc12def9453b90b5440fb660b49f34306b3
bingneef <bingsteup@gmail.com>
2022-11-09 11:46:33 +0100

    Feat: Remove footer and error message Feat: Add base docker-compose file

--- dashboard/helpers/config.py
+++ dashboard/helpers/config.py
@@ -4,11 +4,19 @@ import os
 
 def set_page_config(**kwargs):
     st.set_page_config(**kwargs)
+
+    custom_style = """
+        <style>
+            footer {display: none !important}
+        </style>
+    """
+    st.markdown(custom_style, unsafe_allow_html=True)
     
     if os.getenv("ENV") == "PRODUCTION":
         hide_menu_style = """
-                <style>
-                #MainMenu {visibility: hidden;}
-                </style>
-                """
+            <style>
+                #MainMenu {display: none}
+                .stException pre {display: none}
+            </style>
+        """
         st.markdown(hide_menu_style, unsafe_allow_html=True)
