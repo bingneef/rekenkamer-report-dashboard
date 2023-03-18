253dcc0cd15f9cf618ebefb1f71506bf11a0d31c
Bing Steup <bingsteup@gmail.com>
2023-03-18 19:32:34 +0100

    Fix: build for linux/amd64

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,3 +1,3 @@
 VERSION=${1:-'latest'}
-docker build . -f Dockerfile -t bingneef/rekenkamer-report-dashboard:${VERSION}
+docker build . --platform linux/amd64 -f Dockerfile -t bingneef/rekenkamer-report-dashboard:${VERSION}
 docker push bingneef/rekenkamer-report-dashboard:${VERSION}
